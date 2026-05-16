4d6373c8581ce0b254e49dfbfd238e5eefd5d4d2
yaoyu.jia <1620082624@qq.com>
2026-05-16 21:44:35 +0800

    Adapt codebase to ROS2 Jazzy (Ubuntu 24.04)

---
+++ src/core/build/playgrounds/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jyy/workspace/EPSILON_ROS2/src/core/playgrounds
+# Build directory: /home/jyy/workspace/EPSILON_ROS2/src/core/build/playgrounds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
